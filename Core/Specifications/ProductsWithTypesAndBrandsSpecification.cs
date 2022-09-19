6dc42dc381cd17a8f19832f6e6340270d34ab5a4
JonathanZ-10 <nzamora2000@gmail.com>
2022-09-19 08:28:40 -0400

    End of section 12

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -7,6 +7,10 @@ namespace Core.Specifications
 {
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
+        public ProductsWithTypesAndBrandsSpecification()
+        {
+        }
+
         public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productParams) : base(x=> 
             (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
             (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) && 
