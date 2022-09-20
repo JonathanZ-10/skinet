9b805b4694a700a0d85a894f3674653c3536f756
JonathanZ-10 <nzamora2000@gmail.com>
2022-09-20 08:57:23 -0400

    End of section 18

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -34,12 +34,30 @@ namespace Infrastructure.Data
 
         public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
         {
-            return await ApplySpecification(spec).ToListAsync();
+            var query = ApplySpecification(spec);
+
+            return await query.ToListAsync();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
         }
 
         public async Task<int> CountAsync(ISpecification<T> spec)
         {
             return await ApplySpecification(spec).CountAsync();
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
         }
 
         private IQueryable<T> ApplySpecification(ISpecification<T> spec)
